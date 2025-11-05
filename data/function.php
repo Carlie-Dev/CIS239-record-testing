7a4c721192c0c2db888eeb0131b4d85c08c1185a
PETERS <cp0973791@dt050565.otc.edu>
2025-11-05 09:02:12 -0600

    new structure layout

--- data/function.php
+++ data/function.php
@@ -2,27 +2,30 @@
 // data/function.php
     require 'db.php';
     
+    // Fetch all formats from the database
     function formats_all(): array {
         $pdo = get_pdo();
         $stmt = $pdo->query('SELECT * FROM formats ORDER BY name DESC');
         return $stmt->fetchAll();
     }
 
+    // Fetch all records with their formats from the database
     function records_all(): array {
         $pdo = get_pdo();
         $stmt = $pdo->query('SELECT * FROM records JOIN formats ON records.format_id = formats.id');
         return $stmt->fetchAll();
     }
 
-    // function insert_record(): void {
-    //     $title = "Actung Baby";
-    //     $artist = "U2";
-    //     $price = 60.0;
-    //     $format_id= 1;
-    //     $genre_id = 1;
+    // Insert a new record into the database
+    function insert_record(): void {
+        $title = "Actung Baby";
+        $artist = "U2";
+        $price = 60.0;
+        $format_id= 1;
+        $genre_id = 1;
     
-    //     $pdo = get_pdo();
-    //     $stmt = $pdo->prepare('INSERT INTO records (title, artist, price, format_id, genre_id) VALUES (:title, :artist, :price, :format_id, :genre_id)');
-    //     $stmt->execute([$title, $artist, $price, $format_id, $genre_id]);
-    // }
+        $pdo = get_pdo();
+        $stmt = $pdo->prepare('INSERT INTO records (title, artist, price, format_id, genre_id) VALUES (:title, :artist, :price, :format_id, :genre_id)');
+        $stmt->execute([$title, $artist, $price, $format_id, $genre_id]);
+    }
 ?>
